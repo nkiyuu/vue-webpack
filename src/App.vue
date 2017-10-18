0ee8f7b279af5352f56df18b931b74357be3bf0a
YayaWatanabe <yaya.w.1026@gmail.com>
2017-10-18 17:35:30 +0900

    use vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,8 @@
 <template>
-  <head>
-    <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' rel="stylesheet">
-    <link href="https://unpkg.com/vuetify/dist/vuetify.min.css" rel="stylesheet">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
-  </head>
   <body>
-  <b-navbar toggleable fixed="md" type="dark" variant="info">
-    <b-nav-toggle target="hd-navbar-content"></b-nav-toggle>
 
-    <b-collapse is-nav id="hd-navbar-content">
-      <b-nav is-nav-bar>
-        <b-nav-item to="/">TestFrontPage</b-nav-item>
-        <!-- These links need to defer to the browser navigator -->
-        <!-- ignore the url prefixing... it's for a reverse proxy, not a vue-router thing -->
-        <b-nav-item to="/test/">Test</b-nav-item>
-        <b-nav-item to="/todo/">ToDo</b-nav-item>
-        </b-nav-item>
-      </b-nav>
-    </b-collapse>
-  </b-navbar>
-
-  <div id = "app">
     <router-view/>
   </div>
-
   </body>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,16 +1,21 @@
 
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 import config from './config'
 import * as VueGoogleMaps from 'vue2-google-maps'
-
+//
 // import 'bootstrap/dist/css/bootstrap.css'
 // import 'bootstrap-vue/dist/bootstrap-vue.css'
 // import BootstrapVue from 'bootstrap-vue'
 // Vue.use(BootstrapVue)
+
+import Vuetify from 'vuetify'
+Vue.use(Vuetify)
+import('/Users/yayawatanabe/node_modules/vuetify/dist/vuetify.min.css')
 
 Vue.config.productionTip = false
 
